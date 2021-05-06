e3ee8315bd4d3c870134db7c0e3d34ee7324113f
John Knutson <john.m.knutson@gmail.com>
2021-05-05 20:01:30 -0500

    Fsnotify (#2)
    * initial
    
    * add bits
    
    * add main stuff
    
    * update
    
    * adding some stuff
    
    * dupe detection half working (doesnt upload when it should)
    
    * almost working, not uploading though?
    
    Co-authored-by: John M. Knutson <john.knutson@novu.com>

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/jknutson/go-media-uploader
+
+go 1.16
+
+replace github.com/jknutson/go-udev => /Users/john.knutson/go/src/github.com/jknutson/go-udev
+
+require (
+	github.com/aws/aws-sdk-go v1.38.32
+	github.com/fsnotify/fsnotify v1.4.9
+	github.com/jknutson/go-udev v0.0.0-00010101000000-000000000000
+	github.com/kr/pretty v0.2.1
+	github.com/pilebones/go-udev v0.0.0-20210126000448-a3c2a7a4afb7
+)
---
+++ main.go
@@ -0,0 +1,167 @@
+package main
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/fsnotify/fsnotify"
+)
+
+// TODO: make object / Type for passing around s3manager and s3 svc
+var (
+	s3Bucket      string = "vandelay-media"
+	s3EndpointUrl string = "s3.us-west-000.backblazeb2.com"
+	s3Region      string = "us-west-000" // TODO: parse from Endpoint?
+	watchPath     string = "./mount"
+	s3Client      *s3.S3
+	s3Uploader    *s3manager.Uploader
+)
+
+func objectExists(objectKey string) (bool, error) {
+	input := &s3.HeadObjectInput{
+		Bucket: aws.String(s3Bucket),
+		Key:    aws.String(objectKey),
+	}
+
+	_, err := s3Client.HeadObject(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case "NotFound":
+				return false, nil
+			default:
+				return false, err
+			}
+		}
+	}
+	return true, nil
+}
+
+func uploadFile(localPath string) error {
+	relPath, err := filepath.Rel(watchPath, localPath)
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Open(localPath)
+	if err != nil {
+		return err
+	}
+
+	upParams := &s3manager.UploadInput{
+		Bucket: &s3Bucket,
+		Key:    aws.String(relPath),
+		Body:   file,
+	}
+
+	exists, err := objectExists(relPath)
+	if err != nil {
+		return err
+	}
+
+	if exists {
+		log.Printf("object exists, not uploading: %s\n", relPath)
+		return nil
+	} else {
+		log.Printf("uploading %s -> s3://%s/%s\n", localPath, s3Bucket, relPath)
+	}
+
+	_, err = s3Uploader.Upload(upParams)
+	return err
+}
+
+func processDir(folderPath string) error {
+	files, err := filesToUpload(folderPath)
+	if err != nil {
+		return err
+	}
+	for _, file := range files {
+		statInfo, err := os.Stat(file)
+		if err != nil {
+			// TODO: catch individual errors, attempt processing other files
+			return err
+		}
+		if statInfo.Mode().IsRegular() {
+			log.Printf("%s\n", file)
+			err := uploadFile(file)
+			if err != nil {
+				log.Printf("uploadFile error: %s\n", err)
+			}
+		}
+	}
+	return nil
+}
+
+func filesToUpload(folderPath string) ([]string, error) {
+	var files []string
+	err := filepath.Walk(folderPath,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			files = append(files, path)
+			return nil
+		})
+	if err != nil {
+		return nil, err
+	}
+	return files, nil
+}
+
+func main() {
+	sess, err := session.NewSession(&aws.Config{
+		Endpoint: aws.String(s3EndpointUrl),
+		Region:   aws.String(s3Region),
+	})
+
+	s3Client = s3.New(sess)
+	s3Uploader = s3manager.NewUploader(sess)
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				if event.Op&fsnotify.Create == fsnotify.Create {
+					statInfo, err := os.Stat(event.Name)
+					if err != nil {
+						log.Fatalf("error: %s\n", err)
+					}
+					if statInfo.IsDir() {
+						log.Printf("directory created: %s\n", event.Name)
+						err := processDir(event.Name)
+						if err != nil {
+							log.Printf("error: %s\n", err)
+						}
+					}
+				}
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	err = watcher.Add(watchPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	<-done
+}
